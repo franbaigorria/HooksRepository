2af025696d3cb29fde7a1c499c97dc07cc479810
Francisco Baigorria <franbaigorria21@gmail.com>
2021-06-11 17:38:15 -0300

    primer commit

---
+++ useFetch.js
@@ -0,0 +1,33 @@
+import { useEffect, useRef, useState } from 'react';
+
+export const useFetch = (url) => {
+  const isMount = useRef(true);
+
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    return () => {
+      isMount.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    setState({ data: null, loading: true, error: null });
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        if (isMount.current) {
+          setState({
+            loading: false,
+            error: null,
+            data,
+          });
+        }
+      });
+  }, [url]);
+  return state;
+};
